be1b1c31cf98e5ae7dcf7b69516121c65e10786a
David Vinazza <david.vinazza@gmail.com>
2014-12-29 14:55:44 -0300

    Modo debug. Aparte MOVISTAR cambio la interfaz

--- credito-movistar.py
+++ credito-movistar.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
+# Modo debug mediante environment variable
+from os import getenv
+
 from pyvirtualdisplay import Display
 from selenium import webdriver
-from bs4 import BeautifulSoup
 
 from credenciales import usuario, clave
 
-display = Display(visible=0, size=(800, 600))
+visible = getenv('VISIBLE') or 0
+display = Display(visible=visible, size=(800, 600))
 display.start()
 
-print "Iniciando navegador..."
+
 driver = webdriver.Firefox()
 driver.get("https://online.movistar.com.ar/")
 
-print "Iniciando sesion en movistar..."
 elem = driver.find_element_by_id("userNamesmall")
 elem.send_keys(usuario)
 elem = driver.find_element_by_id("password-clear")
@@ -21,22 +23,17 @@ elem.send_keys(clave)
 elem = driver.find_element_by_name("LoginForm")
 elem.submit()
 
-print "Obteniendo datos de la factura..."
-elem = driver.find_element_by_id("miFactura")
-soup = BeautifulSoup(elem.get_attribute("innerHTML"))
-spans = soup.findAll('span')
-factura = float(spans[1].text[1:].replace(',', '.'))
+driver.get("https://online.movistar.com.ar/autogestion/secured/owner/implLoadPrepaidOverview.do")
+for e in driver.find_elements_by_tag_name("span"):
+    if "$" in e.text:
+        break
 
-print "La factura va a ser por $ %s" % factura
+saldo = float(e.text[1:].replace(',', '.'))
+print saldo
 
-print "Obteniendo datos sobre el saldo..."
-driver.get("https://online.movistar.com.ar/altamira/consultaSaldo.do")
-
-elem = driver.find_element_by_id("avisoSaldo")
-soup = BeautifulSoup(elem.get_attribute("innerHTML"))
-saldo = float(soup.find('strong').text[1:].replace(',', '.'))
-
-print "Tengo $ %s de saldo" % saldo
+if getenv("EXTENDIDO"):
+    for e in driver.find_elements_by_tag_name("table"):
+        print e.text
 
 driver.close()  # cierro el navegador
 display.stop()  # termino el display virtual
